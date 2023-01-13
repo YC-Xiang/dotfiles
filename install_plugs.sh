a3b6d116e509c851d7537f512f738b3309f166a3
yucheng_xiang <yucheng_xiang@realsil.com.cn>
2023-01-13 17:41:56 +0800

    update install shell script

---
+++ install_plugs.sh
@@ -0,0 +1,20 @@
+path=~/.vim/pack/vendor/start
+
+if [ ! -d "$path" ];
+then
+echo "mkdir -p $path"
+mkdir -p $path
+fi
+
+cd $path
+
+if [ ! -d $path/ctrlp ];
+then
+    git clone --depth=1 https://github.com/ctrlpvim/ctrlp.vim.git $path/ctrlp
+fi
+
+if [ ! -d $path/ack.vim ];
+then
+    git clone --depth=1 https://github.com/mileszs/ack.vim.git $path/ack.vim
+fi
+
