12db4235f55ff9948649796df769372fce1e3523
yucheng_xiang <yucheng_xiang@realsil.com.cn>
2023-02-07 17:24:03 +0800

    fix install.sh
    Signed-off-by: yucheng_xiang <yucheng_xiang@realsil.com.cn>

--- install.sh
+++ install.sh
@@ -1,12 +1,15 @@
 cur_dir=$(pwd)
+files=(".vimrc" ".tmux.conf")
 
-if [ -f ~/.vimrc ]; then
-	mv ~/.vimrc $cur_dir/old_files/
+if [ ! -d ${cur_dir}/old_files ]; then
+	mkdir -p old_files
 fi
-ln -s $cur_dir/vim/.vimrc ~/.vimrc
 
+for file in ${files[@]}
+do
+	if [ -f ~/${file} ]; then
+		mv ~/${file} ${cur_dir}/old_files/
+	fi
+	ln -s ${cur_dir}/vim/${file} ~/${file}
+done
 
-if [ -f ~/.tmux.conf ]; then
-	mv ~/.tmux.conf $cur_dir/old_files/
-fi
-ln -s $cur_dir/.tmux.conf ~/.tmux.conf
