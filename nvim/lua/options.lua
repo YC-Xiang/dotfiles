278ea0072d02e0ff6ff80c1d00857cb5f1f2ed2c
yucheng_xiang <yucheng_xiang@realsil.com.cn>
2025-09-09 17:15:01 +0800

    update neovim configs

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -4,6 +4,8 @@ vim.opt.mouse = 'a'
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.cursorline = true
+vim.opt.splitbelow = true
+vim.opt.splitright = true
 
 vim.opt.incsearch = true
 vim.opt.hlsearch = true
