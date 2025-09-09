1096c9c8b820e60fdf61504d702c92e3816ae39a
YC-Xiang <846735552@qq.com>
2025-09-09 21:56:58 +0800

    update nvim configs

---
+++ nvim/lua/keymaps.lua
@@ -0,0 +1,9 @@
+-- Resize with arrows
+-- delta: 2 lines
+vim.keymap.set("n", "<C-Up>", ":resize -2<CR>", opts)
+vim.keymap.set("n", "<C-Down>", ":resize +2<CR>", opts)
+vim.keymap.set("n", "<C-Left>", ":vertical resize -2<CR>", opts)
+vim.keymap.set("n", "<C-Right>", ":vertical resize +2<CR>", opts)
+
+-- nvimTree
+vim.keymap.set('n', '<F2>', ':NvimTreeToggle<CR>', opts)
