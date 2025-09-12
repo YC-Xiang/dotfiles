455dee332bc806e0618e10ba164e44d89d59f768
yucheng_xiang <yucheng_xiang@realsil.com.cn>
2025-09-12 18:00:06 +0800

    update

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -1,4 +1,22 @@
+local opts = {
+	noremap = true;
+	silent = true;
+}
+
+
 vim.g.mapleader = " "
+
+vim.keymap.set("n", "<Up>", "<Nop>", opts)
+vim.keymap.set("n", "<Down>", "<Nop>", opts)
+vim.keymap.set("n", "<Left>", "<Nop>", opts)
+vim.keymap.set("n", "<Right>", "<Nop>", opts)
+vim.keymap.set("i", "<Up>", "<Nop>", opts)
+vim.keymap.set("i", "<Down>", "<Nop>", opts)
+vim.keymap.set("i", "<Left>", "<Nop>", opts)
+vim.keymap.set("i", "<Right>", "<Nop>", opts)
+
+
+vim.keymap.set("i", "jj", "<Esc>", opts)
 
 -- Resize with arrows
 -- delta: 2 lines
@@ -9,3 +27,9 @@ vim.keymap.set("n", "<C-Right>", ":vertical resize +2<CR>", opts)
 
 -- nvimTree
 vim.keymap.set('n', "<F2>", ":NvimTreeToggle<CR>", opts)
+
+-- Telescope
+vim.keymap.set('n', '<leader>ff', ":Telescope find_files<CR>", { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>fg', ":Telescope live_grep<CR>", { desc = 'Telescope live grep' })
+vim.keymap.set('n', '<leader>fb', ":Telescope buffers<CR>", { desc = 'Telescope buffers' })
+vim.keymap.set('n', '<leader>fh', ":Telescope help_tags<CR>", { desc = 'Telescope help tags' })
