a63f60583cd9ae73b3a03aa0c0fcf24d90709d5d
yucheng_xiang <yucheng_xiang@realsil.com.cn>
2025-09-26 16:59:12 +0800

    update

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -2,6 +2,7 @@ vim.opt.clipboard = 'unnamedplus'
 vim.opt.mouse = 'a'
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
+--vim.opt.background = 'light'
 
 vim.opt.number = true
 vim.opt.relativenumber = true
