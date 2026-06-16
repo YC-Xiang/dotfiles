295cd0eea7bd5ea97389ade2d1b089d9b0c1c5f3
yucheng_xiang <yucheng_xiang@realsil.com.cn>
2026-06-16 11:32:35 +0800

    feat(nvim): enable bufferline and multicursor
    Enable bufferline with Shift+H/L keymaps to cycle buffers.
    Enable multicursor.nvim and uncomment line-skip and match-add/skip
    bindings. Add PLUGINS.md documenting all installed plugins.
    
    Signed-off-by: yucheng_xiang <yucheng_xiang@realsil.com.cn>

--- nvim/lua/plugins/multicursor.nvim.lua
+++ nvim/lua/plugins/multicursor.nvim.lua
@@ -1,6 +1,6 @@
 return {
 	"jake-stewart/multicursor.nvim",
-    enabled = false,
+    enabled = true,
 	branch = "1.0", 
     config = function()
         local mc = require("multicursor-nvim")
@@ -11,14 +11,14 @@ return {
         -- Add or skip cursor above/below the main cursor.
         set({"n", "x"}, "<Up>", function() mc.lineAddCursor(-1) end)
         set({"n", "x"}, "<Down>", function() mc.lineAddCursor(1) end)
-        -- set({"n", "x"}, "<leader><Up>", function() mc.lineSkipCursor(-1) end)
-        -- set({"n", "x"}, "<leader><Down>", function() mc.lineSkipCursor(1) end)
+        set({"n", "x"}, "<leader><Up>", function() mc.lineSkipCursor(-1) end)
+        set({"n", "x"}, "<leader><Down>", function() mc.lineSkipCursor(1) end)
 
         -- Add or skip adding a new cursor by matching word/selection
         set({"n", "x"}, "<leader>n", function() mc.matchAddCursor(1) end)
         set({"n", "x"}, "<leader>s", function() mc.matchSkipCursor(1) end)
-        -- set({"n", "x"}, "<leader>N", function() mc.matchAddCursor(-1) end)
-        -- set({"n", "x"}, "<leader>S", function() mc.matchSkipCursor(-1) end)
+        set({"n", "x"}, "<leader>N", function() mc.matchAddCursor(-1) end)
+        set({"n", "x"}, "<leader>S", function() mc.matchSkipCursor(-1) end)
 
         -- Add and remove cursors with control + left click.
         -- set("n", "<c-leftmouse>", mc.handleMouse)
